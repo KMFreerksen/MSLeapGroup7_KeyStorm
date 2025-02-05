437d2f29c570baae7712c11bc9dcd86961e1d884
Ryan Moses <t-ryanmoses@microsoft.com>
2025-02-05 17:02:03 -0500

    USER-1139 Configured base interfaces/classes 	- Displays Output and Reads Input 	- TASK 1124 complete

---
+++ KeyStorm/KeyStorm/Interfaces/IInputProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyStorm.Interfaces
+{
+    /// <summary>
+    /// The IInputProvider interface, represents something that provides inputs
+    /// </summary>
+    public interface IInputProvider
+    {
+        /// <summary>
+        /// Read the input
+        /// </summary>
+        /// <returns>The input</returns>
+        string Read();
+    }
+
+}
