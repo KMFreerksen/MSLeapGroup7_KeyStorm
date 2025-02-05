f1223ef558fe55ad3edb5154026234461db9548b
Skip Thompson <t-skthompson@microsoft.com>
2025-02-05 17:18:12 -0500

    task1125 - Added file for random phrases

---
+++ KeyStorm/KeyStorm/LoadText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyStorm
+{
+    public class LoadText
+    {
+        // need a method that I can call in the program.cs file that outputs the Here we read the txt file
+        public void Load()
+        {
+            Console.WriteLine("Here we read the txt file");
+        }
+    }
+}
