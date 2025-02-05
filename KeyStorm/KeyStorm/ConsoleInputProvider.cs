08d05cbd8823158945579b8310c899e7b42cac39
Ryan Moses <t-ryanmoses@microsoft.com>
2025-02-05 18:06:12 -0500

    Adding Display logic to interfaces 	- IOutputProvider and ConsoleOutputProvider

--- KeyStorm/KeyStorm/ConsoleInputProvider.cs
+++ KeyStorm/KeyStorm/ConsoleInputProvider.cs
@@ -21,6 +21,4 @@ namespace KeyStorm
             return Console.ReadLine();
         }
     }
-    {
-    }
 }
--- KeyStorm/KeyStorm/Program.cs
+++ KeyStorm/KeyStorm/Program.cs
@@ -1,13 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-namespace KeyStorm
+﻿namespace KeyStorm
 {
     class Program
     {
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to KeyStorm!");
+            GameClock.StartCountDown(30);
         }
     }
 }
