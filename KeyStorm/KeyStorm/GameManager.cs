d4189e5af437464418c27b60f98f889fe90b1394
Katie Freerksen <t-kfreerksen@microsoft.com>
2025-02-05 15:54:29 -0600

    create GameManager and GameState files

---
+++ KeyStorm/KeyStorm/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyStorm
+{
+    // Game Manager class to hold the main game loop and logic
+    public class GameManager
+    {
+
+        // TODO create an input provider to handle user input
+        // TODO create an output provider to handle game output
+
+        public GameState GameState { get; private set; }
+    }
+}
